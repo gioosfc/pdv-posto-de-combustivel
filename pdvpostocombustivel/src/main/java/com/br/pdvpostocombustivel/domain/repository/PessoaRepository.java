8cbe511742b95b41d31eb76426535d214b121bb1
gioosfc <giovannaalice2002@gmail.com>
2025-09-16 22:09:25 -0300

    Aula 1609

--- pdvpostocombustivel/src/main/java/com/br/pdvpostocombustivel/domain/repository/PessoaRepository.java
+++ pdvpostocombustivel/src/main/java/com/br/pdvpostocombustivel/domain/repository/PessoaRepository.java
@@ -3,7 +3,6 @@ package com.br.pdvpostocombustivel.domain.repository;
 import com.br.pdvpostocombustivel.domain.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 public interface PessoaRepository  extends JpaRepository<Pessoa, Integer> {
